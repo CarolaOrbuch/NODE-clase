f466b854370b88918dfd4473917f4c782bcc4f50
CarolaOrbuch <101721194+CarolaOrbuch@users.noreply.github.com>
2022-09-19 10:42:55 -0300

    ex1
    ejercicio 1 - nueva guia

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require ('express');
 const app = express(); 
-const port = 9000; //creando una constane port con num (9000 se suele usar para back)
-//linkeando una ruta con una funcion
+const port = 9000; 
+const menu = require('./menu.json');
+app.use(express.json());
 
 //callback, crear rutas
-app.get("/", (req, res) => { //llamamos a get que es una funcion, 
-    //cuando esuches una request get en esta ruta (/), devolve "Hello World!""
-    res.send("Hello World!");
+app.get("/menu/", (req, res) => { 
+    res.json(menu);
 });
 
-// prender el servidor, escuchar al puerto y cuando se prenda mostrar que empezo a andar.
+app.get("/menu/:id", (req, res) => {
+    const id = req.params.id;
+
+    
+});
+
 app.listen(port,() => {
     console.log(`> Server running on port ${port}`);
 });
